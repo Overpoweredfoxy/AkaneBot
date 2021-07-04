52cd534cc8a6a2e61cb77d6ba0f636575c65b836
Overpoweredfoxy <72989848+Overpoweredfoxy@users.noreply.github.com>
2021-07-04 16:22:58 +0100

    Update Akane.js

--- Akane.js
+++ Akane.js
@@ -5,12 +5,32 @@ const config = require('./configs/config.json')
 const fs = require('fs')
 const { Database } = require('quickmongo')
 //const db = new Database(config.database)
+const mongoose = require('mongoose')
+
+
 
 
 
 //handlers + client
 
 const client = new Discord.Client();
+//client.commands = new Discord.collection()
+//client.events = new Discord.collection()
+
+//["command_handler", "event_handler"].forEach((handler) => {
+//    require(`./handlers/${handler}`)(client, Discord);
+//})
+
+mongoose.connect(config.MONGODB_SRV, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+
+}).then(() => {
+    console.log("connected to database")
+}).catch(err => {
+    console.log(err)
+})
 
 
 //events
@@ -22,8 +42,11 @@ client.on('ready',  () => {
 
 //chat
 client.on('message', async (message) => {
-    
+
 })
+
+
+
 
 
 client.login(config.token)
